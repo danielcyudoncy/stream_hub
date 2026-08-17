db1db117afddfaf5b3d1f8212b734651e2a95a49
Daniel Udoncy <113753369+danielcyudoncy@users.noreply.github.com>
2026-08-17 18:26:56 +0100

    fixed the movies screens

--- lib/modules/search/search_hub_binding.dart
+++ lib/modules/search/search_hub_binding.dart
@@ -1,11 +1,22 @@
 import 'package:get/get.dart';
+import '../../../data/repositories/catalog_repository.dart';
+import '../../../data/repositories/history_repository.dart';
 import 'search_hub_controller.dart';
 
 class SearchHubBinding extends Bindings {
   @override
   void dependencies() {
     if (!Get.isRegistered<SearchHubController>()) {
-      Get.lazyPut<SearchHubController>(() => SearchHubController());
+      Get.lazyPut<SearchHubController>(
+        () => SearchHubController(
+          catalogRepository: Get.isRegistered<CatalogRepository>()
+              ? Get.find<CatalogRepository>()
+              : null,
+          historyRepository: Get.isRegistered<HistoryRepository>()
+              ? Get.find<HistoryRepository>()
+              : null,
+        ),
+      );
     }
   }
 }
--- lib/shared/widgets/tv_focusable.dart
+++ lib/shared/widgets/tv_focusable.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import '../../core/theme/app_radius.dart';
 import '../../core/helpers/platform_helper.dart';
+import '../../core/theme/app_radius.dart';
 
 class TvFocusable extends StatefulWidget {
   final Widget child;
@@ -29,41 +29,46 @@ class _TvFocusableState extends State<TvFocusable> {
 
   @override
   Widget build(BuildContext context) {
+    if (!PlatformHelper.isTV) {
+      return GestureDetector(
+        onTap: widget.onTap,
+        behavior: HitTestBehavior.opaque,
+        child: widget.child,
+      );
+    }
+
     final colorScheme = Theme.of(context).colorScheme;
     final focusColor = widget.focusColor ?? colorScheme.primary;
     final borderRadius = widget.borderRadius ?? AppRadius.medium;
 
-    return Focus(
-      onFocusChange: (focused) {
-        if (mounted) {
-          setState(() => _hasFocus = focused);
+    return FocusableActionDetector(
+      onShowFocusHighlight: (show) {
+        if (mounted && _hasFocus != show) {
+          setState(() => _hasFocus = show);
         }
       },
-      child: AnimatedContainer(
-        duration: widget.duration,
-        decoration: BoxDecoration(
-          borderRadius: borderRadius,
-          border: _hasFocus
-              ? Border.all(color: focusColor, width: 3.0)
-              : null,
-          boxShadow: _hasFocus
-              ? [
-                  BoxShadow(
-                    color: focusColor.withValues(alpha: 0.35),
-                    blurRadius: 20.0,
-                    offset: const Offset(0, 6),
-                  ),
-                ]
-              : null,
-        ),
-        child: GestureDetector(
-          onTap: widget.onTap,
-          behavior: HitTestBehavior.opaque,
-          child: AnimatedScale(
-            scale: _hasFocus && PlatformHelper.isTV ? widget.scale : 1.0,
-            duration: widget.duration,
-            child: widget.child,
+      mouseCursor: SystemMouseCursors.click,
+      child: GestureDetector(
+        onTap: widget.onTap,
+        behavior: HitTestBehavior.opaque,
+        child: AnimatedContainer(
+          duration: widget.duration,
+          decoration: BoxDecoration(
+            borderRadius: borderRadius,
+            border: _hasFocus
+                ? Border.all(color: focusColor, width: 3.0)
+                : null,
+            boxShadow: _hasFocus
+                ? [
+                    BoxShadow(
+                      color: focusColor.withValues(alpha: 0.35),
+                      blurRadius: 20.0,
+                      offset: const Offset(0, 6),
+                    ),
+                  ]
+                : null,
           ),
+          child: widget.child,
         ),
       ),
     );
